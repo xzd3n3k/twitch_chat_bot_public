7f7be08a6a9ad751ece9548421261eece18e9849
Zdeněk Němec <xzdeneknemec@outlook.cz>
2022-06-04 11:05:44 +0200

    removed main from class file

--- bot.py
+++ bot.py
@@ -265,16 +265,3 @@ class Twitch:
 
     # def send_raw(self, string):
     #     self.irc.send((string+'\r\n').encode('utf-8'))
-
-
-def main():
-    acc = Twitch('oauth:', '')
-    # testing
-    acc.connect()
-    acc.loop_for_messages()
-    # acc.force_disconnect()
-    # acc.close_socket()
-
-
-if __name__ == '__main__':
-    main()
